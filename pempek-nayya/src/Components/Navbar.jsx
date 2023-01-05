e2778d4734a5beccb30229cbe99add6f6d8de894
fenny wilriani <fenny.wilriani@gmail.com>
2023-01-05 17:35:13 +0100

    adding responsive navbar

--- pempek-nayya/src/Components/Navbar.jsx
+++ pempek-nayya/src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Collapse from 'react-bootstrap/Collapse';
 
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+
   return (
     <div className="container d-flex flex-column flex-md-row">
         <nav className="navbar navbar-expand-md navbar-light d-flex flex-md-column">
@@ -12,6 +15,7 @@ export default function Navbar() {
                 <img src="../assets/logo.png" alt="logo" width="100" height="100" />
             </a>
             <button
+              onClick={() => setOpen(!open)}
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
@@ -22,6 +26,7 @@ export default function Navbar() {
             >
                 <span className="navbar-toggler-icon"></span>
             </button>
+            <Collapse in={open}>
             <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
                 <ul className="navbar-nav w-100 d-flex flex-md-column text-center text-md-end">
                     <li>
@@ -38,6 +43,8 @@ export default function Navbar() {
                     </li>
                 </ul>
             </div>
+            </Collapse>
+            
 
         </nav>
         {/* <main className="ps-0 ps-md-5 flex-grow-1">
