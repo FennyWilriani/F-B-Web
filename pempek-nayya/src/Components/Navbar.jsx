43a5875f98034831ffe52609091796f374dabd84
fenny wilriani <fenny.wilriani@gmail.com>
2023-01-05 16:56:48 +0100

    adding assets

---
+++ pempek-nayya/src/Components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+export default function Navbar() {
+  return (
+    <div className="container d-flex flex-column flex-md-row">
+        <nav className="navbar navbar-expand-md navbar-light d-flex flex-md-column">
+            <a href="logo">
+                <img src="../assets/logo.png" alt="logo" width="100" height="100" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle Navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
+                <ul className="navbar-nav w-100 d-flex flex-md-column text-center text-md-end">
+                    <li>
+                        <a href="menu" className="nav-link" aria-current="page">MENU</a>
+                    </li>
+                    <li>
+                        <a href="event" className="nav-link">EVENT</a>
+                    </li>
+                    <li>
+                        <a href="gallery" className="nav-link">GALLERY</a>
+                    </li>
+                    <li>
+                        <a href="contact" className="nav-link">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+        {/* <main className="ps-0 ps-md-5 flex-grow-1">
+            <img src="../assets/hero-img.jpg" alt="logo" max-width="100%" height="100%"/>
+        </main> */}
+    </div>
+  )
+}
