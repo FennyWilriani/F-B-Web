33442cc8474eff8ebd8f9a4bf933ea8a96ba8155
fenny wilriani <fenny.wilriani@gmail.com>
2023-01-06 22:49:23 +0100

    finished navbar

--- pempek-nayya/src/Components/Navbar.jsx
+++ pempek-nayya/src/Components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import './Navbar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Css/Navbar.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
 
 
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
   return (
-    <div className="container d-flex flex-column flex-md-row">
-        <nav className="navbar navbar-expand-md navbar-light d-flex flex-md-column">
-            <a href="logo">
-                <img src="../assets/logo.png" alt="logo" width="100" height="100" />
+    <div className="container-fluid flex-column d-flex flex-md-row ">
+        <nav className="navbar navbar-expand-md d-flex flex-md-column p-3">
+            <a href="#logo">
+                <img src="../assets/logo.png" alt="logo" width="120" height="auto" />
             </a>
             <button
               onClick={() => setOpen(!open)}
@@ -27,29 +28,40 @@ export default function Navbar() {
                 <span className="navbar-toggler-icon"></span>
             </button>
             <Collapse in={open}>
-            <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
-                <ul className="navbar-nav w-100 d-flex flex-md-column text-center text-md-end">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav d-flex flex-md-column text-center">
                     <li>
-                        <a href="menu" className="nav-link" aria-current="page">MENU</a>
+                        <a href="#menu" className="nav-link fw-bold fs-5 mt-3 text-danger" aria-current="page">MENU</a>
                     </li>
                     <li>
-                        <a href="event" className="nav-link">EVENT</a>
+                        <a href="#event" className="nav-link fw-bold fs-5 text-danger">EVENT</a>
                     </li>
                     <li>
-                        <a href="gallery" className="nav-link">GALLERY</a>
+                        <a href="#gallery" className="nav-link fw-bold fs-5 text-danger">GALLERY</a>
                     </li>
                     <li>
-                        <a href="contact" className="nav-link">CONTACT</a>
+                        <a href="#contact" className="nav-link mb-3 fw-bold fs-5 text-danger">CONTACT</a>
+                    </li>
+                    <li>
+                        <Button variant="warning" href="https://wa.me/6282219190684" className='btn-primary shadow m-5 fw-bold text-light'>ORDER ONLINE</Button>{' '}
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/pempeknayya50/">
+                            <i className="bi bi-instagram heading h3"/>
+                        </a>
+                        <p className='text-danger'>3404 PALM AVE,<br/>GARUT, JABAR 92154, INDONESIA<br/>(619) 600-3161</p>
                     </li>
                 </ul>
             </div>
             </Collapse>
-            
-
         </nav>
-        {/* <main className="ps-0 ps-md-5 flex-grow-1">
-            <img src="../assets/hero-img.jpg" alt="logo" max-width="100%" height="100%"/>
-        </main> */}
+        <main className='ps-0 flex-grow-1'>
+            <div
+            className='masthead' 
+            style={{ backgroundImage: "url(/assets/image.jpg)" }}>
+                Hello World
+            </div>
+        </main>
     </div>
   )
 }
